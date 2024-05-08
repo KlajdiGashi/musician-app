640248b49d85de93525684aa22322818a4818229
Klajdi Gashi <klajdi.gashi@student.uni-pr.edu>
2024-05-08 17:50:52 +0200

    feat:changed a file

--- routes/musician.js
+++ routes/musician.js
@@ -9,6 +9,12 @@ router.get('/health', (req, res) => {
   res.status('200').send("Status: ok!");
 });
 
+
+router.get('/securitycheck', (req, res) => {
+  res.status('200').send("Status: ok!");
+});
+
+
 // retrieve all musicians from data store
 router.get('/all', (req, res) => {
   const { musician } = req.app.locals;
